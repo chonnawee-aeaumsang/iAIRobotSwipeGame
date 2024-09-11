cc263b3041abed23a0ce73ceac4782b66469edd4
Chonnawee aeaumsang <chonnawee_aeaumsang@cmu.ac.th>
2024-09-11 15:36:43 +0700

    Push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const TOKEN = "7498251188:AAH7qY1omTujNBtBKghu_d6ypT7cflfDf9U"; // Replace with your bot token
+const TOKEN = "7498251188:AAE_tTlO0FEquxuzf3lotNmNOninhb12biI"; // Replace with your bot token
 const webhookUrl = "https://robot-swipei-ai.vercel.app/api/webhook"; // This should match your deployed function URL
 
 const bot = new TelegramBot(TOKEN, { polling: false });
